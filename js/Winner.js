5eca29b3093af385e936c4b70ecdad7f9df1dc3d
drnlab <hola@drnlab.net>
2017-01-21 23:47:40 +0100

    Ni idea

--- js/Winner.js
+++ js/Winner.js
@@ -7,21 +7,33 @@ Waves.Winner.prototype = {
  
   preload: function() {
  
-    },
+  },
  
   create: function() {
+  
       this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-	  this.game.add.sprite( 0, 0, 'winner', 0 );
+	  this.game.add.sprite( 0, 0, "winner", 0 );
+	  
+	  var style = { font: "bold 120px Arial", fill: "#7e5f33", boundsAlignH: "center", boundsAlignV: "middle" };	  
+      this.scoreP1Text = this.game.add.text( 490, 175, scoreP1, style );	  
+      this.scoreP2Text = this.game.add.text( 640, 175, scoreP2, style );
+
+	  this.game.add.sprite( 0, 300, "winnerHojas" );
+	  
 	  this.input.onDown.add( this.onMouseDownCallback, this );
 	  
-	  var style = { font: "bold 75px Arial", fill: "#7e5f33", boundsAlignH: "center", boundsAlignV: "middle" };	  
-      this.scoreP1Text = this.game.add.text( 48, 70, scoreP1, style );	  
-      this.scoreP2Text = this.game.add.text( 1110, 70, scoreP2, style );  
+	  if ( winner == 1 ) {
+		this.game.add.sprite( 0, 65, "winnerWL" );
+	  } else if ( winner == 2 ) {
+		this.game.add.sprite( 0, 65, "winnerLW" );
+	  } else {
+		this.game.add.sprite( 0, 65, "winnerDD" );
+	  }
 
   },
  
-  onMouseDownCallback: function() {
-     this.state.start('Menu');
+  onMouseDownCallback: function( x, y ) {
+     this.state.start( 'Menu' );
   }
     
 };
